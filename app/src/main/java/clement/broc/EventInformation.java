493c65082ed39c7d08a16d9e273ea908ae40621f
Clément Vion <clement.vion49@gmail.com>
2017-05-30 13:47:56 +0200

    add events in database

---
+++ app/src/main/java/clement/broc/EventInformation.java
@@ -0,0 +1,16 @@
+package clement.broc;
+
+/**
+ * Created by clement on 30/05/2017.
+ */
+
+public class EventInformation {
+
+    public String name;
+    public String address;
+
+    public EventInformation(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
--- app/src/main/java/clement/broc/Tab4.java
+++ app/src/main/java/clement/broc/Tab4.java
@@ -6,16 +6,65 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import static com.google.android.gms.internal.zzs.TAG;
 
 /**
  * Created by clement on 28/05/2017.
  */
 
-public class Tab4 extends Fragment {
+public class Tab4 extends Fragment implements View.OnClickListener {
+
+    private EditText editTextEventName;
+    private EditText editTextEventAddress;
+    private Button buttonEventAdd;
+
+    private DatabaseReference databaseReference;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState) {
-        return inflater.inflate(R.layout.tab4, container, false);
+        View rootView = inflater.inflate(R.layout.tab4, container, false);
+        rootView.setTag(TAG);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        editTextEventName = (EditText) rootView.findViewById(R.id.editTextEventName);
+        editTextEventAddress = (EditText) rootView.findViewById(R.id.editTextEventAddress);
+        buttonEventAdd = (Button) rootView.findViewById(R.id.buttonEventAdd);
+
+        buttonEventAdd.setOnClickListener(this);
+
+        return rootView;
+    }
+
+
+    public void addEvent() {
+        String name = editTextEventName.getText().toString().trim();
+        String address = editTextEventAddress.getText().toString().trim();
+
+        EventInformation eventInformation = new EventInformation(name, address);
+
+        DatabaseReference postReference = databaseReference.child("events").push();
+
+        postReference.setValue(eventInformation);
+
+        Toast.makeText(getActivity(), "Evènement enregistré", Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        if(view == buttonEventAdd){
+            addEvent();
+        }
+
     }
 }
