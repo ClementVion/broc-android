2e6007fc0f1208081a488a671587c4a1185ca8d6
Clément Vion <clement.vion49@gmail.com>
2017-06-06 21:09:32 +0200

    fallback for profile options

--- app/src/main/java/clement/broc/Tab3.java
+++ app/src/main/java/clement/broc/Tab3.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,9 +18,12 @@ import static com.google.android.gms.internal.zzs.TAG;
  * Created by clement on 28/05/2017.
  */
 
-public class Tab3 extends Fragment {
+public class Tab3 extends Fragment implements View.OnClickListener {
 
     private TextView profileEmail;
+    private TextView profileSettingsAccount;
+    private TextView profileSettingsEmail;
+    private TextView profileSettingsEvents;
 
     private FirebaseAuth firebaseAuth;
 
@@ -32,10 +36,34 @@ public class Tab3 extends Fragment {
         firebaseAuth = FirebaseAuth.getInstance();
 
         profileEmail = (TextView) rootView.findViewById(R.id.profileEmail);
+        profileSettingsAccount = (TextView) rootView.findViewById(R.id.profileSettingsAccount);
+        profileSettingsEmail = (TextView) rootView.findViewById(R.id.profileSettingsEmail);
+        profileSettingsEvents = (TextView) rootView.findViewById(R.id.profileSettingsEvents);
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
         profileEmail.setText(user.getEmail());
 
+        profileSettingsAccount.setOnClickListener(this);
+        profileSettingsEmail.setOnClickListener(this);
+        profileSettingsEvents.setOnClickListener(this);
+
         return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(view == profileSettingsAccount) {
+            Toast.makeText(getContext(), "Cette fonctionnalité sera bientôt disponible !", Toast.LENGTH_SHORT).show();
+        }
+
+        if(view == profileSettingsEmail) {
+            Toast.makeText(getContext(), "Cette fonctionnalité sera bientôt disponible !", Toast.LENGTH_SHORT).show();
+        }
+
+        if(view == profileSettingsEvents) {
+            Toast.makeText(getContext(), "Cette fonctionnalité sera bientôt disponible !", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
