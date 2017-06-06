7456cf0b5339018181d434dc52cbf26566cf5933
Clément Vion <clement.vion49@gmail.com>
2017-06-06 13:16:25 +0200

    profile

--- app/src/main/java/clement/broc/Tab3.java
+++ app/src/main/java/clement/broc/Tab3.java
@@ -6,6 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static com.google.android.gms.internal.zzs.TAG;
 
 /**
  * Created by clement on 28/05/2017.
@@ -13,9 +19,23 @@ import android.view.ViewGroup;
 
 public class Tab3 extends Fragment {
 
+    private TextView profileEmail;
+
+    private FirebaseAuth firebaseAuth;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState) {
-        return inflater.inflate(R.layout.tab3, container, false);
+        View rootView = inflater.inflate(R.layout.tab3, container, false);
+        rootView.setTag(TAG);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        profileEmail = (TextView) rootView.findViewById(R.id.profileEmail);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        profileEmail.setText(user.getEmail());
+
+        return rootView;
     }
 }
